c42f62cd5c33a4327ad33f6781ef81b354764931
喵的云 <17775513826@163.com>
2019-05-25 17:56:41 +0800

    第一次提交

---
+++ house_server/gethouselist.php
@@ -0,0 +1,24 @@
+<?php
+	// 获取所有借款数据
+	// 加载跨域文件
+	require_once "./connect.php";
+	require_once './cross.php';
+	
+	$city = $_GET['city'];
+	
+	/* 
+		1. 获取用户数据
+		2. 执行sql
+		3. 进行逻辑判断，响应
+	 */
+	
+	$data = mysqli_query($conn,"select * from house where city='$city'");	
+	$arr = [];
+	/* 循环数据，并打印出来 */
+	while($row=mysqli_fetch_assoc($data)){
+		array_push($arr, $row);
+	}
+	
+	echo json_encode($arr);
+	
+?>
